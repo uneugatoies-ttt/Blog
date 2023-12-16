510d06d594a78e2f8fab889e286ac60b9cf8762b
ttt Uneugatoies <t3u33t333@gmail.com>
2023-12-16 10:44:37 +0900

    Test 임시 주석 처리

--- src/test/java/com/example/blog/BlogApplicationTests.java
+++ src/test/java/com/example/blog/BlogApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.blog;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class BlogApplicationTests {
 
+	/*
 	@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
