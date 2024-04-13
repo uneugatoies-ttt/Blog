70c3da4d9894691a40492a6079b6d95a73454b6a
ttt Uneugatoies <t3u33t333@gmail.com>
2024-04-13 17:21:32 +0900

    HealthCheckController 주석 추가

--- src/main/java/com/example/blog/controller/HealthCheckController.java
+++ src/main/java/com/example/blog/controller/HealthCheckController.java
@@ -3,6 +3,9 @@ package com.example.blog.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+	-> HealthCheckController는 AWS 환경에서 이 애플리케이션이 동작하는지 확인하는 데에 사용되는 controller이다.
+*/
 @RestController
 public class HealthCheckController {
 	
